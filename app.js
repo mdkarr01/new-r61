e58e474302fba8fc5543e5fdf007b0407a4cd46c
Michael Karr <mdkarr01@gmail.com>
2018-05-03 17:41:11 -0500

    making posts changes for blog entries

--- app.js
+++ app.js
@@ -26,11 +26,15 @@ mongoose.Promise = global.Promise;
 const databaseUri = process.env.MONGODB_URI;
 
 mongoose
-  .connect(databaseUri, { useMongoClient: true })
+  .connect(databaseUri, {
+    useMongoClient: true
+  })
   .then(() => console.log(`Database connected`))
   .catch(err => console.log(`Database connection error: ${err.message}`));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -55,7 +59,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -70,6 +74,10 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 //   console.log("The Route 61 Server Has Started!");
 // });
 
+<<<<<<< Updated upstream
 app.listen(3000 || process.env.PORT, process.env.IP, function() {
+=======
+app.listen(3000 || process.env.PORT, process.env.IP, function () {
+>>>>>>> Stashed changes
   console.log("The Route 61 Server Has Started!");
 });
