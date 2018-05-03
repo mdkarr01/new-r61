41049eb08e0d827ef8c53994c89d5b68c71b2727
Michael Karr <mdkarr01@gmail.com>
2018-05-03 13:44:01 -0500

    update port

--- app.js
+++ app.js
@@ -66,6 +66,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000 || process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
   console.log("The Route 61 Server Has Started!");
 });
