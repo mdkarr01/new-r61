80a5d1ddb6273e0743fc941c6592bf94b06ad01a
Michael Karr <mdkarr01@gmail.com>
2018-05-03 13:22:50 -0500

    changed new.ejs for uploading image

--- app.js
+++ app.js
@@ -67,5 +67,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(3000 || process.env.PORT, process.env.IP, function() {
-  console.log("The YelpCamp Server Has Started!");
+  console.log("The Route 61 Server Has Started!");
 });
