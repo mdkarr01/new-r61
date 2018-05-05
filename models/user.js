f602e47c844b724d8517dc401f7ff8e8296e013b
Michael Karr <mdkarr01@gmail.com>
2018-05-05 15:54:40 -0500

    changed back to img-fluid

--- models/user.js
+++ models/user.js
@@ -2,11 +2,13 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    isAdmin: {type: Boolean, default: false}
+  email: String,
+  username: String,
+  password: String,
+  isAdmin: { type: Boolean, default: false },
+  avatar: String
 });
 
-UserSchema.plugin(passportLocalMongoose)
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
