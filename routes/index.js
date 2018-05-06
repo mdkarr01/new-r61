a3fefab18c65312b1a313bdf3d876e8302e645c4
Michael Karr <mdkarr01@gmail.com>
2018-05-06 10:23:59 -0500

    smaller feature pic and added email to pw reset

--- routes/index.js
+++ routes/index.js
@@ -103,13 +103,13 @@ router.post('/forgot', function (req, res, next) {
       var smtpTransport = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-          user: 'learntocodeinfo@gmail.com',
+          user: 'michaelkarrnet@gmail.com',
           pass: process.env.GMAILPW
         }
       });
       var mailOptions = {
         to: user.email,
-        from: 'learntocodeinfo@gmail.com',
+        from: 'michaelkarrnet@gmail.com',
         subject: 'Node.js Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
@@ -179,7 +179,7 @@ router.post('/reset/:token', function (req, res) {
       var smtpTransport = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-          user: 'learntocodeinfo@gmail.com',
+          user: 'michaelkarrnet@gmail.com',
           pass: process.env.GMAILPW
         }
       });
