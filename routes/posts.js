a1d57e89ef8c76fd79279049c02b5bbe115d8ac0
Michael Karr <mdkarr01@gmail.com>
2018-05-07 14:00:40 -0500

    getting closer to post upload. photo works

--- routes/posts.js
+++ routes/posts.js
@@ -4,16 +4,21 @@ var Posts = require("../models/posts");
 var Comment = require("../models/comment");
 var middleware = require("../middleware");
 var geocoder = require("geocoder");
-var { isLoggedIn, checkUserPost, checkUserComment, isAdmin } = middleware; // destructuring assignment
+var {
+  isLoggedIn,
+  checkUserPost,
+  checkUserComment,
+  isAdmin
+} = middleware; // destructuring assignment
 
 var request = require("request");
 var multer = require("multer");
 var storage = multer.diskStorage({
-  filename: function(req, file, callback) {
+  filename: function (req, file, callback) {
     callback(null, Date.now() + file.originalname);
   }
 });
-var imageFilter = function(req, file, cb) {
+var imageFilter = function (req, file, cb) {
   // accept image files only
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
     return cb(new Error("Only image files are allowed!"), false);
@@ -38,15 +43,14 @@ function escapeRegex(text) {
 }
 
 //INDEX - show all posts
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   if (req.query.search && req.xhr) {
     const regex = new RegExp(escapeRegex(req.query.search), "gi");
     // Get all posts from DB
-    Posts.find(
-      {
+    Posts.find({
         name: regex
       },
-      function(err, allPosts) {
+      function (err, allPosts) {
         if (err) {
           console.log(err);
         } else {
@@ -56,7 +60,7 @@ router.get("/", function(req, res) {
     );
   } else {
     // Get all posts from DB
-    Posts.find({}, function(err, allPosts) {
+    Posts.find({}, function (err, allPosts) {
       if (err) {
         console.log(err);
       } else {
@@ -74,11 +78,11 @@ router.get("/", function(req, res) {
 });
 
 //CREATE - add new post to DB
-router.post("/", middleware.isLoggedIn, upload.single("image"), function(
+router.post("/", middleware.isLoggedIn, upload.single("image"), function (
   req,
   res
 ) {
-  cloudinary.uploader.upload(req.file.path, function(result) {
+  cloudinary.uploader.upload(req.file.path, function (result) {
     // add cloudinary url for the image to the post object under image property
     req.body.post.image = result.secure_url;
     // add author to post
@@ -86,7 +90,7 @@ router.post("/", middleware.isLoggedIn, upload.single("image"), function(
       id: req.user._id,
       username: req.user.username
     };
-    Post.create(req.body.post, function(err, post) {
+    Posts.create(req.body.post, function (err, post) {
       if (err) {
         req.flash("error", err.message);
         return res.redirect("back");
@@ -97,16 +101,16 @@ router.post("/", middleware.isLoggedIn, upload.single("image"), function(
 });
 
 //NEW - show form to create new post
-router.get("/new", isLoggedIn, function(req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
   res.render("posts/new");
 });
 
 // SHOW - shows more info about one post
-router.get("/:id", function(req, res) {
+router.get("/:id", function (req, res) {
   //find the post with provided ID
   Posts.findById(req.params.id)
     .populate("comments")
-    .exec(function(err, foundPosts) {
+    .exec(function (err, foundPosts) {
       if (err || !foundPosts) {
         console.log(err);
         req.flash("error", "Sorry, that post does not exist!");
@@ -121,7 +125,7 @@ router.get("/:id", function(req, res) {
 });
 
 // EDIT - shows edit form for a post
-router.get("/:id/edit", isLoggedIn, checkUserPost, function(req, res) {
+router.get("/:id/edit", isLoggedIn, checkUserPost, function (req, res) {
   //render edit template with that post
   res.render("posts/edit", {
     post: req.post
@@ -129,8 +133,8 @@ router.get("/:id/edit", isLoggedIn, checkUserPost, function(req, res) {
 });
 
 // PUT - updates post in the database
-router.put("/:id", function(req, res) {
-  geocoder.geocode(req.body.location, function(err, data) {
+router.put("/:id", function (req, res) {
+  geocoder.geocode(req.body.location, function (err, data) {
     var lat = data.results[0].geometry.location.lat;
     var lng = data.results[0].geometry.location.lng;
     var location = data.results[0].formatted_address;
@@ -144,11 +148,10 @@ router.put("/:id", function(req, res) {
       lng: lng
     };
     Posts.findByIdAndUpdate(
-      req.params.id,
-      {
+      req.params.id, {
         $set: newData
       },
-      function(err, post) {
+      function (err, post) {
         if (err) {
           req.flash("error", err.message);
           res.redirect("back");
@@ -162,19 +165,18 @@ router.put("/:id", function(req, res) {
 });
 
 // DELETE - removes post and its comments from the database
-router.delete("/:id", isLoggedIn, checkUserPost, function(req, res) {
-  Comment.remove(
-    {
+router.delete("/:id", isLoggedIn, checkUserPost, function (req, res) {
+  Comment.remove({
       _id: {
         $in: req.post.comments
       }
     },
-    function(err) {
+    function (err) {
       if (err) {
         req.flash("error", err.message);
         res.redirect("/");
       } else {
-        req.post.remove(function(err) {
+        req.post.remove(function (err) {
           if (err) {
             req.flash("error", err.message);
             return res.redirect("/");
