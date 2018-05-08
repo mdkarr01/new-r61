03502a6359a7be27d478b4cfd9a11487e45a5254
Michael Karr <mdkarr01@gmail.com>
2018-05-08 16:48:28 -0500

    all kinds of shit

--- middleware/index.js
+++ middleware/index.js
@@ -45,6 +45,25 @@ module.exports = {
       req.flash('error', 'This site is now read only thanks to spam and trolls.');
       res.redirect('back');
     }
+  },
+  truncate: function (str, len) {
+    if (str.length > len && str.length > 0) {
+      var new_str = str + " ";
+      new_str = str.substr(0, len);
+      new_str = str.substr(0, new_str.lastIndexOf(" "));
+      new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
+      return new_str + '...';
+    }
+    return str;
+  },
+  stripTags: function (input) {
+    return input.replace(/<(?:.|\n)*?>/gm, '');
+  },
+  formatDate: function (date, format) {
+    return moment(date).format(format);
+  },
+  select: function (selected, options) {
+    return options.fn(this).replace(new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace(new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
   }
   // isSafe: function (req, res, next) {
   //   if (req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
