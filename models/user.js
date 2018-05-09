25c6db75364dd3bceffd802bd60c8aa068d2265a
Michael Karr <mdkarr01@gmail.com>
2018-05-09 11:07:27 -0500

    fixed login with email in user.js

--- models/user.js
+++ models/user.js
@@ -1,44 +1,7 @@
-// var mongoose = require("mongoose");
-// var passportLocalMongoose = require("passport-local-mongoose");
-
-// var UserSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true
-//   },
-//   password: {
-//     type: String
-//   },
-//   firstName: {
-//     type: String
-//   },
-//   lastName: {
-//     type: String
-//   },
-//   avatar: {
-//     type: String,
-//     required: false
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   isAdmin: {
-//     type: Boolean,
-//     default: false
-//   }
-// });
-
-// UserSchema.plugin(passportLocalMongoose);
-
-// module.exports = mongoose.model("User", UserSchema);
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-  usernameField: 'email',
-  usernameQueryFields: ['email'],
   username: {
     type: String,
     unique: true,
@@ -62,6 +25,10 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-UserSchema.plugin(passportLocalMongoose)
+// UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email',
+  usernameQueryFields: ['email']
+});
 
 module.exports = mongoose.model("User", UserSchema);
