c491bfc177a4efdaf287c03d0e580e95ddf1af86
Michael Karr <mdkarr01@gmail.com>
2018-05-17 12:43:31 -0500

    should work on dev

--- routes/posts.js
+++ routes/posts.js
@@ -154,64 +154,64 @@ router.get("/:id/edit", isLoggedIn, middleware.checkUserPost, function (req, res
   });
 });
 
-// router.put("/:id", upload.single('image'),
-//   function (req, res) {
-//     Posts.findById(req.params.id, async function (err, post) {
-//       if (err) {
-//         req.flash("error", err.message);
-//         res.redirect("back");
-//       } else {
-//         if (req.file) {
-//           try {
-//             await cloudinary.v2.uploader.destroy(post.imageId);
-//             var result = await cloudinary.v2.uploader.upload(req.file.path);
-//             post.imageId = result.public_id;
-//             post.image = result.secure_url;
-//           } catch (err) {
-//             req.flash("error", err.message);
-//             return res.redirect("back");
-//           }
-//         }
-//         post.title = req.body.title;
-//         post.body = req.body.body;
-//         post.tag1 = req.body.tag1;
-//         post.tag2 = req.body.tag2;
-//         post.tag3 = req.body.tag3;
-//         post.tag4 = req.body.tag4;
-//         post.status = req.body.status;
-//         post.alt = req.body.alt;
-//         post.isPrimary = req.body.isPrimary;
-//         post.save();
-//         req.flash("success", "Successfully Updated This Post.");
-//         res.redirect("/posts/" + post._id);
-//       }
-//     });
-//   });
-
-router.put("/:id", function (req, res) {
-  var newData = {
-    title: req.body.title,
-    image: req.body.image,
-    body: req.body.body,
-    tag1: req.body.tag1,
-    tag2: req.body.tag2,
-    tag3: req.body.tag3,
-    status: req.body.status,
-    iPrimary: req.body.iPrimary,
-    alt: req.body.alt,
-  };
-  Posts.findByIdAndUpdate(req.params.id, {
-    $set: newData
-  }, function (err, post) {
-    if (err) {
-      req.flash("error", err.message);
-      res.redirect("back");
-    } else {
-      req.flash("success", "Successfully Updated!");
-      res.redirect("/posts/" + post._id);
-    }
+router.put("/:id", upload.single('image'),
+  function (req, res) {
+    Posts.findById(req.params.id, async function (err, post) {
+      if (err) {
+        req.flash("error", err.message);
+        res.redirect("back");
+      } else {
+        if (req.file) {
+          try {
+            await cloudinary.v2.uploader.destroy(post.imageId);
+            var result = await cloudinary.v2.uploader.upload(req.file.path);
+            post.imageId = result.public_id;
+            post.image = result.secure_url;
+          } catch (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+          }
+        }
+        post.title = req.body.title;
+        post.body = req.body.body;
+        post.tag1 = req.body.tag1;
+        post.tag2 = req.body.tag2;
+        post.tag3 = req.body.tag3;
+        post.tag4 = req.body.tag4;
+        post.status = req.body.status;
+        post.alt = req.body.alt;
+        post.isPrimary = req.body.isPrimary;
+        post.save();
+        req.flash("success", "Successfully Updated This Post.");
+        res.redirect("/posts/" + post._id);
+      }
+    });
   });
-});
+
+// router.put("/:id", function (req, res) {
+//   var newData = {
+//     post: req.body.title,
+//     post: req.body.image,
+//     post: req.body.body,
+//     post: req.body.tag1,
+//     post: req.body.tag2,
+//     post: req.body.tag3,
+//     post: req.body.status,
+//     post: req.body.iPrimary,
+//     post: req.body.alt,
+//   };
+//   Posts.findByIdAndUpdate(req.params.id, {
+//     $set: newData
+//   }, function (err, post) {
+//     if (err) {
+//       req.flash("error", err.message);
+//       res.redirect("back");
+//     } else {
+//       req.flash("success", "Successfully Updated!");
+//       res.redirect("/posts/" + post._id);
+//     }
+//   });
+// });
 
 // router.delete('/:id', isLoggedIn, function (req, res) {
 //   Posts.findById(req.params.id, async function (err, post) {
