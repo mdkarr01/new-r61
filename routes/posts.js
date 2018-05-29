25364b126368d4c2f6b4831237e580a1aae061be
Michael Karr <mdkarr01@gmail.com>
2018-05-29 13:03:10 -0500

    added edit:id back in w/o async

--- routes/posts.js
+++ routes/posts.js
@@ -194,40 +194,39 @@ router.get("/:id/edit", isLoggedIn, middleware.checkUserPost, function (req, res
   });
 });
 
-// router.put("/:id", upload.single('image'),
-//   function (req, res) {
-//     Posts.findById(req.params.id, async function (err, post) {
-//       if (err) {
-//         req.flash("error", err.message);
-//         res.redirect("back");
-//       } else {
-//         if (req.file) {
-//           try {
-//             await cloudinary.v2.uploader.destroy(post.imageId);
-//             var result = await cloudinary.v2.uploader.upload(req.file.path);
-//             post.imageId = result.public_id;
-//             post.image = result.secure_url;
-//             // eval(require('locus'));
-//           } catch (err) {
-//             req.flash("error", err.message);
-//             return res.redirect("back");
-//           }
-//         }
-//         post.title = req.body.title;
-//         post.body = req.body.body;
-//         post.tag1 = req.body.tag1;
-//         post.tag2 = req.body.tag2;
-//         post.tag3 = req.body.tag3;
-//         post.tag4 = req.body.tag4;
-//         post.status = req.body.status;
-//         post.alt = req.body.alt;
-//         post.isPrimary = req.body.isPrimary;
-//         post.save();
-//         req.flash("success", "You have successfully updated this post.");
-//         res.redirect("/posts/" + post._id);
-//       }
-//     });
-//   });
+router.put("/:id", upload.single('image'), (req, res) => {
+  Posts.findById(req.params.id, (err, post) => {
+    if (err) {
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else {
+      if (req.file) {
+        try {
+          cloudinary.v2.uploader.destroy(post.imageId);
+          var result = cloudinary.v2.uploader.upload(req.file.path);
+          post.imageId = result.public_id;
+          post.image = result.secure_url;
+          // eval(require('locus'));
+        } catch (err) {
+          req.flash("error", err.message);
+          return res.redirect("back");
+        }
+      }
+      post.title = req.body.title;
+      post.body = req.body.body;
+      post.tag1 = req.body.tag1;
+      post.tag2 = req.body.tag2;
+      post.tag3 = req.body.tag3;
+      post.tag4 = req.body.tag4;
+      post.status = req.body.status;
+      post.alt = req.body.alt;
+      post.isPrimary = req.body.isPrimary;
+      post.save();
+      req.flash("success", "You have successfully updated this post.");
+      res.redirect("/posts/" + post._id);
+    }
+  });
+});
 
 // router.put("/:id", function (req, res) {
 //   var newData = {
